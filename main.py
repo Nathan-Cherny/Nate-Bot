f7a7a1064cc6e963b3a3d61cbc5bb3dfec3dee3a
Nathan C <84670821+Nathan-Cherny@users.noreply.github.com>
2023-12-18 19:47:26 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 import json
 
-KEY = "MTE4MjczNDc3MDczNjc0NjY1Ng.GUT99X.iJZU4hR7gyYB8BsJReo7BKZWIIYuKhNEbFaeUk"
+KEY = ""
 
 intents = discord.Intents.default()
 intents.message_content = True
