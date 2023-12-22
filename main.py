34ed9bef054479f625db68abd8e9049d1a34426e
Natann826 <natec3632@gmail.com>
2023-12-22 13:25:08 -0500

    more stuf

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from dotenv import load_dotenv
 import os
 import requests
 import random
+from pytube import YouTube
+import ffmpeg
 
 load_dotenv()
 
@@ -42,6 +44,18 @@ class JSON:
             data[str(userId)][attribute] = newValue
             json.dump(data, f, indent=4)
 
+def testIfCursed(user):
+    data = JSON.readJson('userData.json')
+    if data[str(user.id)]['cursed']:
+        return True
+    return False
+
+def getAudioSourceUrlFromYouTubeLink(url):
+    yt = YouTube(url)
+    return yt.streams.filter(only_audio=True)[-1]
+
+# events ------------------------
+
 @bot.event
 async def on_ready():
     await bot.change_presence(status=discord.Status.online, activity=discord.Game(f"Hi Nate"))
@@ -60,14 +74,18 @@ async def on_message(message):
         await message.author.ban(reason="FISH.")
     else:   
         await bot.process_commands(message)
+    
+# commands ----------------------
 
-@bot.command()
+@bot.command(help='Curses a user, and prevents them from talking. Do !curse <@user>')
 async def curse(ctx, *, user: discord.Member):
 
     if user.id == 1182734770736746656:
         await ctx.send(f"Fuck you {ctx.author.mention}, you can't curse me.")
         await ctx.send(f"!curse {ctx.author.mention}")
-        JSON.updateUserData(user.id, "cursed", True)
+
+        JSON.updateUserData(ctx.author.id, "cursed", True)
+
         await ctx.send(f"{ctx.author.mention} is cursed. Anything they say will be immediately deleted.")
         return
 
@@ -78,7 +96,7 @@ async def curse(ctx, *, user: discord.Member):
 
     await ctx.send(f"{user.mention} is cursed. Anything they say will be immediately deleted.")
 
-@bot.command()
+@bot.command(help='Uncurses a user. See curse.')
 async def uncurse(ctx, *, user: discord.Member):
     if user.id == ctx.author.id:
         return
@@ -90,7 +108,7 @@ async def uncurse(ctx, *, user: discord.Member):
 
     await ctx.send(f"{user.mention} is uncursed. You are free from your shackles.")
 
-@bot.command()
+@bot.command(help='Generates a random gif and sends it. Mostly kpop or anime unfortunately.')
 async def randomGif(ctx):
     string = ""
     length = 8
@@ -103,11 +121,33 @@ async def randomGif(ctx):
 
     await ctx.send(url)
 
-def testIfCursed(user):
-    data = JSON.readJson('userData.json')
-    if data[str(user.id)]['cursed']:
-        return True
-    return False
+@bot.command(help='Bot will join the vc of the user who calls this.')
+async def joinvc(ctx):
+    if not ctx.message.author.voice:
+        await ctx.send(f"{ctx.message.author.mention}, you're not in a vc right now")
+        return
+    else:
+        channel = ctx.message.author.voice.channel
+    await channel.connect()
+
+@bot.command(help="If the bot is in a vc, it'll leave it.")
+async def leavevc(ctx):
+    if ctx.voice_client:
+        await ctx.guild.voice_client.disconnect()
+        
+@bot.command(help='Plays the audio from a YouTube url. !play <url>')
+async def playYT(ctx, *, url):
+    if ctx.voice_client:
+        audio = ffmpeg.input(getAudioSourceUrlFromYouTubeLink(url))
+        ctx.voice_client.play(audio.output('music.webm'))
+
+
+url = getAudioSourceUrlFromYouTubeLink("https://www.youtube.com/watch?v=pL9o_BwSN-o")
+
+audio = ffmpeg.input(url)
+
+print(url.url)
+
 
 
 bot.run(KEY)
